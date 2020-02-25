7c8e1f08f91132abeb7a0e1cbc4e23864523b0db
LukHtl2 <lueg229@gmail.com>
2020-02-25 20:39:58 +0100

    Methoden zur Farb und Wertberechnung hinzugefügt

--- Poker/src/PokerProgramm.java
+++ Poker/src/PokerProgramm.java
@@ -1,34 +1,53 @@
 public class PokerProgramm {
 
-	public static int[] ArrayBefüllen(int[] Karten, int anzahlKarten, int anzahlFarbe) {
-		for (int i = 0; i < anzahlKarten * anzahlFarbe; i++) {
-			Karten[i] = i;
+	public static int[] ArrayBefüllen(int[] karten, int anzahlkarten, int anzahlFarbe) {
+		for (int i = 0; i < anzahlkarten * anzahlFarbe; i++) {
+			karten[i] = i;
 		}
-		return Karten;
+		return karten;
 	}
 
-	public static int[] GezogeneKarten(int[] Karten, int[] gezogenekarten, int grenze, int oben, int anzahl) {
+	public static int[] Gezogenekarten(int[] karten, int[] gezogenekarten, int grenze, int anzahl) {
 		for (int h = 0; h < anzahl; h++) {
 			int zufall = (int) (Math.random() * grenze);
-			int test = Karten[oben]; 
-			Karten[oben] = Karten[zufall];
-			Karten[zufall] = test;
-			gezogenekarten[h] = Karten[oben];
-			oben = oben - 1;
+			int test = karten[grenze];
+			karten[grenze] = karten[zufall];
+			karten[zufall] = test;
+			gezogenekarten[h] = karten[grenze];
+			grenze = grenze - 1;
 		}
 		return gezogenekarten;
 	}
 
+	public static int[] Farbe(int[] gezogenekarten, int[] farbe) {
+		for (int j = 0; j < gezogenekarten.length; j++) {
+			int f = gezogenekarten[j] / 13;
+			farbe[j] = f;
+		}
+		return farbe;
+	}
+
+	public static int[] Wert(int[] gezogenekarten, int[] wert) {
+		for (int j = 0; j < gezogenekarten.length; j++) {
+			int w = gezogenekarten[j] % 13;
+			wert[j] = w;
+		}
+		return wert;
+	}
+	
 	public static void main(String[] args) {
 		int anzahlFarbe = 4;
-		int anzahlKarten = 13;
+		int anzahlkarten = 13;
 		int grenze = 51;
-		int oben = 51;
 		int anzahl = 5;
-		int[] Karten = new int[anzahlKarten * anzahlFarbe];
+		int[] karten = new int[anzahlkarten * anzahlFarbe];
 		int[] gezogenekarten = new int[anzahl];
-
-		ArrayBefüllen(Karten, anzahlKarten, anzahlFarbe);
-		GezogeneKarten(Karten,gezogenekarten, grenze, oben, anzahl);
+		int[] werte = new int[anzahl];
+		int[] farbe = new int[anzahl];
+	
+		ArrayBefüllen(karten, anzahlkarten, anzahlFarbe);
+		Gezogenekarten(karten, gezogenekarten, grenze, anzahl);
+		Farbe(gezogenekarten, farbe);
+		Wert(gezogenekarten, werte);
 	}
 }
